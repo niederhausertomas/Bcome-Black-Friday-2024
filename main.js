df82a76420357ccc815bf50d3c1c08a9dd898353
Tomás Niederhauser <78279996+niederhausertomas@users.noreply.github.com>
2024-11-10 10:23:06 +0100

    add fonts

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
 document.querySelector('.calculate').addEventListener('click', function() {
-    // Obtener los valores de los inputs
+
     const price = parseFloat(document.querySelectorAll('.input1')[0].value);
     const frequency = parseFloat(document.querySelectorAll('.input1')[1].value);
 
-    // Verificar que ambos valores sean números válidos y que la frecuencia no sea cero
+
     if (!isNaN(price) && !isNaN(frequency) && frequency !== 0) {
-        // Calcular el resultado de la división
+
         const result = (price / frequency).toFixed(2);
 
-        // Seleccionar todos los elementos con la clase .result y actualizar su contenido
-        const resultElements = document.querySelectorAll('.result');
-        resultElements.forEach(element => {
-            element.textContent = `${result} €`;
+
+        const formattedResult = parseFloat(result).toLocaleString('es-ES', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
         });
 
-        // Mostrar el contenedor de resultados y ocultar el contenedor de inputs
+
+        const resultElements = document.querySelectorAll('.result');
+        resultElements.forEach(element => {
+            element.textContent = `${formattedResult} €`;
+        });
+
+
         document.querySelector('.inputContainer').style.display = 'none';
         document.querySelector('.resultContainer').style.display = 'block';
-    } else {
-        // Mostrar un mensaje de error si hay valores no válidos
-        alert('Por favor ingresa números válidos y asegúrate de que la frecuencia no sea cero.');
     }
+});
+
+
+
+
+document.querySelector('.try').addEventListener('click', function() {
+
+    document.querySelectorAll('.input1')[0].value = '';
+    document.querySelectorAll('.input1')[1].value = '';
+
+    document.querySelector('.inputContainer').style.display = 'block';
+    document.querySelector('.resultContainer').style.display = 'none';
 });
